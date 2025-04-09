998eda3cead5b49ce6ed758b122df4e8fa0c39d2
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-04-09 08:17:46 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import { renderHtml } from "./renderHtml";
+import { renderAbout } from "./aboutHtml";
 
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    const path = url.pathname;
+
+    if (path === "/cart") {
+      return new Response(renderCart(), {
+        headers: { "content-type": "text/html" },
+      });
+    }
+
     const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
     const { results } = await stmt.all();
 
     return new Response(renderHtml(JSON.stringify(results, null, 2)), {
-      headers: {
-        "content-type": "text/html",
-      },
+      headers: { "content-type": "text/html" },
     });
   },
 } satisfies ExportedHandler<Env>;
