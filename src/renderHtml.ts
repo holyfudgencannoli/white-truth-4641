dc539922dbb9a0cf7d253568766ff7045a5e06f7
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-04-09 06:47:27 -0400

    Update renderHtml.ts

--- src/renderHtml.ts
+++ src/renderHtml.ts
@@ -4,8 +4,8 @@ export function renderHtml(content: string) {
 <html lang="en">
 <head>
     <!-- PASTE INTO OUTERMOST FOLDER. NONE OF THE LOCATIONS HAVE BEEN MODIFIED. PLUG N PLAY -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://github.com/holyfudgencannoli/white-truth-4641/blob/main/src/css/style.css">
     <meta charset="UTF-8">
     <title>Page title</title>
 </head>
