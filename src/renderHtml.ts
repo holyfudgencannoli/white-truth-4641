14ed540c82af025a85defc62edf39658d27c5d04
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-04-09 06:44:34 -0400

    Update renderHtml.ts

--- src/renderHtml.ts
+++ src/renderHtml.ts
@@ -1,29 +1,55 @@
 export function renderHtml(content: string) {
   return `
     <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>D1</title>
-        <link rel="stylesheet" type="text/css" href="https://static.integrations.cloudflare.com/styles.css">
-      </head>
+<html lang="en">
+<head>
+    <!-- PASTE INTO OUTERMOST FOLDER. NONE OF THE LOCATIONS HAVE BEEN MODIFIED. PLUG N PLAY -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <meta charset="UTF-8">
+    <title>Page title</title>
+</head>
+<body>
+    <nav id="navbar" class="navbar navbar-expand-lg container-fluid">
+        <div class=" ">
+            <a class="nav-link" href="#"><h1 class="text-center m-3" id="navtitle">American Chic</h1></a>
+        </div>
+        <div class="text-right">
+            <ul class="navlist justify-content-between m-3">
+                <a class="navlink" href="">Home |</a>
+                <a href="">Gallery |</a>
+                <a href="assets/pages/account.html">My Account |</a>
+                <a id="cart" href="assets/pages/cart.html">My Cart</a>
+            </ul>
+        </div>
+    </nav>
+    <br>
+    <div class="container-fluid text-center">
+        <div id="title" class="row">
+            <h3>Welcome to the Home of American Chic!</h3>
+            <p>Take a look through our gallery and contact us when you find what you're looking for</p>
+        </div>
+    </div>
+    <div id="main-content" class="container text-center">
+        <div class="row">
+            <div id="gallery"></div>
+        </div>
+    </div>
+    <div>
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
     
-      <body>
-        <header>
-          <img
-            src="https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/30e0d3f6-6076-40f8-7abb-8a7676f83c00/public"
-          />
-          <h1>🎉 Successfully connected white-truth-4641 to D1</h1>
-        </header>
-        <main>
-          <p>Your D1 Database contains the following data:</p>
-          <pre><code><span style="color: #0E838F">&gt; </span>SELECT * FROM comments LIMIT 3;<br>${content}</code></pre>
-          <small class="blue">
-            <a target="_blank" href="https://developers.cloudflare.com/d1/tutorials/build-a-comments-api/">Build a comments API with Workers and D1</a>
-          </small>
-        </main>
-      </body>
-    </html>
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/cart-function.js"></script>
+    
+</body>
+</html>
+
 `;
 }
